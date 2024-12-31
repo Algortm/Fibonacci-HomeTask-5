c4f257c54cf0a03a39aa01c388240f7f1d5dff4b
Algortm <ilnutskyy@gmail.com>
2024-12-31 10:00:27 +0200

    Refact home work

--- src/main/java/org/example/DynamicFibonacci.java
+++ src/main/java/org/example/DynamicFibonacci.java
@@ -1,20 +1,19 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 class DynamicFibonacci implements FibonacciCalculate{
     @Override
     public long calculate(int n){
-        Map<Integer,Long> arrayMap = new HashMap<>();
+        ArrayList<Long> arrayList = new ArrayList<>();
         if(n<=1){
             return n;
         }
-        if(arrayMap.containsKey(n)){
-            return arrayMap.get(n);
+        arrayList.add(0L);
+        arrayList.add(1L);
+        for(int i = 2; i<=n; i++){
+            arrayList.add(arrayList.get(i-1)+arrayList.get(i-2));
         }
-        long result = calculate(n-1)+calculate(n-2);
-        arrayMap.put(n, result);
-        return result;
+        return arrayList.get(n);
     }
 }
--- src/main/java/org/example/IterationFibonacci.java
+++ src/main/java/org/example/IterationFibonacci.java
@@ -1,22 +1,17 @@
 package org.example;
 
-import java.util.ArrayList;
-
 public class IterationFibonacci implements FibonacciCalculate{
     @Override
     public long calculate(int n){
-        ArrayList<Long> arrayList = new ArrayList<>();
         if(n<=1){
             return n;
         }
-        long result = 2L;
-        arrayList.add(0L);
-        arrayList.add(1L);
-        for(int i = 2; i<=n; i++){
-            arrayList.add(arrayList.get(i-1)+arrayList.get(i-2));
-            result = arrayList.get(i);
-
+        long a = 0, b = 1, c;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
         }
-        return result;
+        return b;
     }
 }
